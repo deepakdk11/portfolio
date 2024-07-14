9853b7b8962f7453d7a700dcba27f006f169852e
deepakdk11 <deepakkumar545424@gmail.com>
2024-07-14 18:09:52 +0530

    update year and New Resume link add

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,5 +1,9 @@
 const ul = document.querySelector("ul");
-const navBar = document.querySelector(".navbar")
+const navBar = document.querySelector(".navbar");
+const copy = document.querySelector(".copyright");
+
+const year = new Date().getFullYear();
+copy.innerHTML = `Copyright © ${year}`
 
 navBar.addEventListener("click", () => {
     ul.classList.toggle("showMenu")
